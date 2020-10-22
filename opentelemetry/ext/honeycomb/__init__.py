e03d8268929533ed6a9a593df9d5e0d753df8560
Paul Osman <paul@honeycomb.io>
2020-10-22 09:49:09 -0500

    Update version
    Co-authored-by: Mike Goldsmith <goldsmith.mike@gmail.com>

--- opentelemetry/ext/honeycomb/__init__.py
+++ opentelemetry/ext/honeycomb/__init__.py
@@ -29,7 +29,7 @@ import opentelemetry.trace as trace_api
 from opentelemetry.sdk.trace.export import SpanExporter, SpanExportResult
 from opentelemetry.trace.status import StatusCanonicalCode
 
-VERSION = '0.5b0'
+VERSION = '0.6b0'
 USER_AGENT_ADDITION = 'opentelemetry-exporter-python/%s' % VERSION
 
 
