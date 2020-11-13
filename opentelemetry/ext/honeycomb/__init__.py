65074ea4633f5293683b20b938e3e18eae5c768c
Liz Fong-Jones <lizf@honeycomb.io>
2020-11-13 08:53:05 -0800

    version bump to v0.15b0 (#14)

--- opentelemetry/ext/honeycomb/__init__.py
+++ opentelemetry/ext/honeycomb/__init__.py
@@ -29,7 +29,7 @@ import opentelemetry.trace as trace_api
 from opentelemetry.sdk.trace.export import SpanExporter, SpanExportResult
 from opentelemetry.trace.status import StatusCode
 
-VERSION = '0.6b0'
+VERSION = '0.15b0'
 USER_AGENT_ADDITION = 'opentelemetry-exporter-python/%s' % VERSION
 
 
